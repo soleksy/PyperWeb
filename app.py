bc6213da702d77d0ffd55d7dcc3719ac4832eb05
soleksy <szy.oleksy@gmail.com>
2024-03-16 15:03:38 +0100

    fix db initialization

--- app.py
+++ app.py
@@ -1,4 +1,9 @@
-from flaskapp import app
+from flaskapp import app, db
+
+def initialize_database():
+    with app.app_context():
+        db.create_all()
 
 if __name__ == '__main__':
+    initialize_database()
     app.run(debug=True)
--- flaskapp/__init__.py
+++ flaskapp/__init__.py
@@ -6,14 +6,7 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = DB_URI + '?check_same_thread=False'
 app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde2aaa0ba245'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-db=SQLAlchemy(app)
 
-@app.before_first_request
-def initialize_database():
-    # Create all tables defined in the models
-    db.create_all()
-    
-    
-
+db = SQLAlchemy(app)
 
 from flaskapp import routes
